3216f8060084ee1d8abcfe52797f0868fda0e064
Victor Anokwuru <vc.anokwuru@gmail.com>
2022-09-22 09:05:28 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar';
+import Home from './components/Home';
+import Products from './components/Products';
+import Product from './components/Product';
+import Contact from './components/Contact';
+import { Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/Products" element={<Products />} />
+        <Route path="/Products/:id" element={<Product />} />
+        <Route path="/Contact" element={<Contact />} />
+      </Routes>
+    </>
   );
 }
 
---
+++ src/components/Contact.jsx
@@ -0,0 +1,47 @@
+import React, { useState } from 'react';
+import './Contact.css';
+import { useRef } from 'react';
+import emailjs from '@emailjs/browser';
+
+const Contact = () => {
+    const form = useRef();
+
+    const [done, setDone] = useState(false)
+
+    const sendEmail = (e) => {
+        e.preventDefault();
+
+        emailjs.sendForm('service_h4f9dl7', 'template_h4cmjne', form.current, 'YmbgAu2M_3T47URsF')
+            .then((result) => {
+                console.log(result.text);
+                setDone(true)
+            }, (error) => {
+                console.log(error.text);
+            });
+    };
+
+    return (
+        <div className="mx-auto pt-5" id="contact">
+            <span>Get in Touch</span>
+            <form ref={form} onSubmit={sendEmail}>
+                <div class="mb-3 form-input">
+                    <label for="exampleFormControlInput1" class="form-label">Name</label>
+                    <input class="form-control" type="text" placeholder="" aria-label="default input example" />
+                </div>
+                <div class="mb-3 form-input">
+                    <label for="exampleFormControlInput1" class="form-label">Email</label>
+                    <input type="email" class="form-control" id="email" placeholder="" />
+                </div>
+                <div class="mb-3 form-input">
+                    <label for="exampleFormControlTextarea1" class="form-label">Message</label>
+                    <textarea class="form-control" id="message" rows="3"></textarea>
+                </div>
+                <input type="submit" value="send" className="button" />
+                <span>{done && "Thanks for contacting me!"}</span>
+            </form>
+            
+        </div>
+    );
+};
+
+export default Contact;
